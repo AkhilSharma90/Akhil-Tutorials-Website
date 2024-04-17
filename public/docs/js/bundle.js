403e35837b3165d56d744750826d6c8b2c16d983
iamvibs <vm11082001@gmail.com>
2024-04-17 14:12:03 +0530

    baseurl added

--- public/docs/js/bundle.js
+++ public/docs/js/bundle.js
@@ -315,7 +315,7 @@ tocBtn.addEventListener('hidden.bs.dropdown', event => {
   var params_default;
   var init_params = __esm({
     "ns-params:@params"() {
-      params_default = { langPath: "//localhost:1313/docs/js/components/" };
+      params_default = { langPath: "http://localhost:1313/tutorials/docs/js/components/" };
     }
   });
 
